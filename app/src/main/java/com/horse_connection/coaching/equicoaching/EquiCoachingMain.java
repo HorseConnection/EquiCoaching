664f3fe6afce46c57604e0aca0de0415b8179eac
HorseConnection <philippevh60@hotmail.fr>
2017-10-20 14:47:17 +0200

    Initialisation

---
+++ app/src/main/java/com/horse_connection/coaching/equicoaching/EquiCoachingMain.java
@@ -0,0 +1,13 @@
+package com.horse_connection.coaching.equicoaching;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class EquiCoachingMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_equi_coaching_main);
+    }
+}
