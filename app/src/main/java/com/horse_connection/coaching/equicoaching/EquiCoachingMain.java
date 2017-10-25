64e684be3ef168a89fe106b4e9e6ee313c362171
HorseConnection <philippevh60@hotmail.fr>
2017-10-25 16:37:44 +0200

    Version 1.2 : Finalisation intégration de EquiFoulées en tant que Lib

--- app/src/main/java/com/horse_connection/coaching/equicoaching/EquiCoachingMain.java
+++ app/src/main/java/com/horse_connection/coaching/equicoaching/EquiCoachingMain.java
@@ -2,10 +2,16 @@ package com.horse_connection.coaching.equicoaching;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
+
+import com.horse_connection.coaching.equifouleeslibrary.MainActivity;
+
+import java.util.List;
 
 public class EquiCoachingMain extends AppCompatActivity {
 
@@ -17,24 +23,29 @@ public class EquiCoachingMain extends AppCompatActivity {
         ctx = getApplicationContext();
 
         setContentView(R.layout.activity_equi_coaching_main);
+        //setTitle(R.string.app_name_EC);
 
         ImageButton btnEquiFoulees = (ImageButton) findViewById(R.id.btnEquiFoulees);
         btnEquiFoulees.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                PackageManager pm;
+                pm = getPackageManager();
+
+                List<ApplicationInfo> packages = pm.getInstalledApplications(0);
+
                 /*
-                Intent myIntent = null;
-                try {
-                    myIntent = new Intent(ctx,Class.forName("com.horse_connection.coaching.equifoulees"));
-                } catch (ClassNotFoundException e) {
-                    e.printStackTrace();
-                }
-                startActivity(myIntent );
-                */
-                Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.horse_connection.coaching.equifoulees");
+                Intent launchIntent = ctx.getPackageManager().getLaunchIntentForPackage("com.horse_connection.coaching.equifoulees");
+
                 if (launchIntent != null) {
-                    startActivity(launchIntent);//null pointer check in case package name was not found
+                    startActivity(launchIntent);
+                } else {
+                    Toast.makeText(ctx, "Package not found", Toast.LENGTH_SHORT).show();
                 }
+                */
+                Intent myIntent = new Intent(v.getContext(), MainActivity.class);
+                startActivityForResult(myIntent, 0);
+
             }
         });
     }
