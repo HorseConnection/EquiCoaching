cd0910000a6592d420fbfe4b73ea89fd5571f6da
HorseConnection <philippevh60@hotmail.fr>
2017-10-26 15:57:13 +0200

    Version 1.2 : EquiCoaching released

--- app/src/main/java/com/horse_connection/coaching/equicoaching/EquiCoachingMain.java
+++ app/src/main/java/com/horse_connection/coaching/equicoaching/EquiCoachingMain.java
@@ -2,16 +2,12 @@ package com.horse_connection.coaching.equicoaching;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import com.horse_connection.coaching.equifouleeslibrary.MainActivity;
-
-import java.util.List;
 
 public class EquiCoachingMain extends AppCompatActivity {
 
@@ -23,30 +19,20 @@ public class EquiCoachingMain extends AppCompatActivity {
         ctx = getApplicationContext();
 
         setContentView(R.layout.activity_equi_coaching_main);
-        //setTitle(R.string.app_name_EC);
 
-        ImageButton btnEquiFoulees = (ImageButton) findViewById(R.id.btnEquiFoulees);
-        btnEquiFoulees.setOnClickListener(new View.OnClickListener() {
+        ImageView imgEquiFoulees = (ImageView) findViewById(R.id.imEquiFoulees);
+
+        imgEquiFoulees.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                PackageManager pm;
-                pm = getPackageManager();
-
-                List<ApplicationInfo> packages = pm.getInstalledApplications(0);
-
-                /*
-                Intent launchIntent = ctx.getPackageManager().getLaunchIntentForPackage("com.horse_connection.coaching.equifoulees");
-
-                if (launchIntent != null) {
-                    startActivity(launchIntent);
-                } else {
-                    Toast.makeText(ctx, "Package not found", Toast.LENGTH_SHORT).show();
-                }
-                */
+                    /*
+                    PackageManager pm;
+                    pm = getPackageManager();
+                    List<ApplicationInfo> packages = pm.getInstalledApplications(0);
+                    */
                 Intent myIntent = new Intent(v.getContext(), MainActivity.class);
                 startActivityForResult(myIntent, 0);
-
-            }
+                }
         });
     }
 }
